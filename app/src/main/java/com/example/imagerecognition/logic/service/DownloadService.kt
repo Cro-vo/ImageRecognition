4fd128ae7c0963f5befbdb4530e5876faa7b314b
Crovo <2316923313@qq.com>
2022-05-21 18:25:17 +0800

    Finish download pic mudule.

---
+++ app/src/main/java/com/example/imagerecognition/logic/service/DownloadService.kt
@@ -0,0 +1,169 @@
+package com.example.imagerecognition.logic.service
+
+import android.Manifest
+import android.app.Activity
+import android.app.Service
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Binder
+import android.os.Build
+import android.os.Environment
+import android.os.IBinder
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.Target
+import com.example.imagerecognition.ImageRecognitionApplication
+import com.example.imagerecognition.ImageRecognitionApplication.Companion.context
+import kotlinx.coroutines.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
+
+class DownloadService : Service() {
+
+    private val mBinder = DownloadBinder()
+
+    class DownloadBinder: Binder() {
+
+        fun startDownload(context: Context, url: String){
+
+            thread {
+                val file = Glide.with(context).load(url).downloadOnly(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL).get()
+                val bitmap = BitmapFactory.decodeFile(file.path)
+
+                runBlocking {
+                    // 兼容29版本上下
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        saveImage29(bitmap, context)
+                    } else {
+                        saveImage(bitmap, context)
+                    }
+                }
+//                Log.d("test", bitmap.toString())
+
+            }
+
+        }
+
+
+        private fun saveImage(toBitmap: Bitmap, context: Context) {
+            val insertImage = MediaStore.Images.Media.insertImage(
+                context.contentResolver, toBitmap, "ImageRecognition Download", "ImageRecognition保存的图片"
+            )
+            if (insertImage.isNotEmpty()) {
+                Toast.makeText(context, "图片保存成功！-${insertImage}", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, "图片保存失败！}", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // 保存图片
+        private suspend fun saveImage29(toBitmap: Bitmap, context: Context) {
+            //开始一个新的进程执行保存图片的操作
+            withContext(Dispatchers.IO) {
+
+                val insertUri = context.contentResolver.insert(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, ContentValues()
+                ) ?: kotlin.run {
+                    showSaveToast(context, "保存失败！")
+                    return@withContext
+                }
+                //使用use可以自动关闭流
+                context.contentResolver.openOutputStream(insertUri).use {
+                    if (toBitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)) {
+                        showSaveToast(context,"保存成功！")
+                        Log.d("test","ok")
+                    } else {
+                        showSaveToast(context, "保存失败！")
+                        Log.d("test","fail")
+                    }
+                }
+            }
+        }
+        private fun showSaveToast(context: Context, showMsg: String) =
+            MainScope().launch {
+                Toast.makeText(context, showMsg, Toast.LENGTH_SHORT).show()
+            }
+
+
+        fun saveMediaToStorage(context: Context,bitmap: Bitmap) {
+            //Generating a file name
+            val filename = "${System.currentTimeMillis()}.jpg"
+
+            //Output stream
+            var fos: OutputStream? = null
+
+            //For devices running android >= Q
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                //getting the contentResolver
+                context?.contentResolver?.also { resolver ->
+
+                    //Content resolver will process the contentvalues
+                    val contentValues = ContentValues().apply {
+
+                        //putting file information in content values
+                        put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                        put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                    }
+
+                    //Inserting the contentValues to contentResolver and getting the Uri
+                    val imageUri: Uri? =
+                        resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+                    //Opening an outputstream with the Uri that we got
+                    fos = imageUri?.let { resolver.openOutputStream(it) }
+                }
+            } else {
+                //These for devices running on android < Q
+                //So I don't think an explanation is needed here
+                val imagesDir =
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                val image = File(imagesDir, filename)
+                fos = FileOutputStream(image)
+            }
+
+            fos?.use {
+                //Finally writing the bitmap to the output stream that we opened
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+//                Log.d("test","save success.")
+            }
+        }
+
+
+
+
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        return mBinder
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/example/imagerecognition/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/imagerecognition/ui/detail/DetailActivity.kt
@@ -1,17 +1,42 @@
 package com.example.imagerecognition.ui.detail
 
+import android.Manifest
+import android.app.Activity
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import com.bumptech.glide.Glide
+import com.example.imagerecognition.ImageRecognitionApplication
 import com.example.imagerecognition.databinding.ActivityDetailBinding
 import com.example.imagerecognition.logic.model.AnimalResponse
 import com.example.imagerecognition.logic.model.PlantResponse
+import com.example.imagerecognition.logic.service.DownloadService
 import com.example.imagerecognition.ui.mainPage.MainActivity
 
 class DetailActivity : AppCompatActivity() {
 
+    // 存放网络图片url
+    lateinit var imgUrl: String
+
+    lateinit var downloadBinder: DownloadService.DownloadBinder
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+            downloadBinder = p1 as DownloadService.DownloadBinder
+            downloadBinder.startDownload(this@DetailActivity, imgUrl)
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+
+        }
+    }
 
     lateinit var binding: ActivityDetailBinding
 
@@ -29,20 +54,60 @@ class DetailActivity : AppCompatActivity() {
             binding.score.text = "相似度：${result.score}"
             binding.description.text = result.baikeInfo.description
 
+            imgUrl = result.baikeInfo.imageURL
+
         } else if (function == MainActivity.PLANT){
-            Log.d("plant", "here")
+//            Log.d("plant", "here")
             val result = intent.getParcelableExtra<PlantResponse.Result>("result")
             Glide.with(this).load(result!!.baikeInfo.imageURL).into(binding.imageView)
             binding.name.text = result.name
             binding.score.text = "相似度：${result.score}"
             binding.description.text = result.baikeInfo.description
 
+            imgUrl = result.baikeInfo.imageURL
+
         }
 
 
+        binding.button.setOnClickListener {
+            requestPermission()
+
+            val intent = Intent(this, DownloadService::class.java)
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+
+
+        }
 //        Log.d("result", "result in  activity ${result}")
 
-
-
     }
+
+
+    /**
+     * request permission for the application
+     */
+    private fun requestPermission() {
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                1
+            )
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                1
+            )
+        } else {
+//            Log.d("test", "-----------------permission ok")
+        }
+    }
+
 }
