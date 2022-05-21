dbb2174a3378ff03e39e99dec4d786399f6d68cb
Crovo <2316923313@qq.com>
2022-05-21 21:20:22 +0800

    finish broadcast.

---
+++ app/src/main/java/com/example/imagerecognition/logic/broadcast/MsgReceiver.kt
@@ -0,0 +1,14 @@
+package com.example.imagerecognition.logic.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MsgReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        Toast.makeText(context, "保存成功！！！", Toast.LENGTH_SHORT).show()
+    }
+}
